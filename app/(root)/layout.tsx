765ecdf8eeffd0dda59a7bf1aabaccb82fba8a1e
Tareq <tareqnmd@gmail.com>
2024-03-26 09:47:37 +0600

    updated

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react';
 
-import Footer from '@/components/Footer';
-import Navbar from '@/components/Navbar';
+import Footer from '@/components/layout/Footer';
+import Navbar from '@/components/layout/Navbar';
 
 const layout = ({ children }: { children: ReactNode }) => {
 	return (
