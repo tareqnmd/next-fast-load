bf76f4a7121f8b56d7cb0af93ffbba57737c448e
Tareq <tareqnmd@gmail.com>
2024-03-26 09:16:11 +0600

    up

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,28 @@
-import './globals.css'
-import type { Metadata } from 'next'
+import '@/styles/global.scss';
+import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'JS Mastery',
-  description: 'JS Mastery Resources',
-  other: {
-    'theme-color': '#0d1117',
-    "color-scheme": "dark only",
-    "twitter:image": 'https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg',
-    "twitter:card": "summary_large_image",
-    "og:url": "jsmastery.pro",
-    "og:image": 'https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg',
-    "og:type": "website",
-  }
-}
+	title: 'JS Mastery',
+	description: 'JS Mastery Resources',
+	other: {
+		'theme-color': '#0d1117',
+		'color-scheme': 'dark only',
+		'twitter:image': 'https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg',
+		'twitter:card': 'summary_large_image',
+		'og:url': 'jsmastery.pro',
+		'og:image': 'https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg',
+		'og:type': 'website',
+	},
+};
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className="min-h-screen bg-black-100 font-poppins">{children}</body>
-    </html>
-  )
+	return (
+		<html lang="en">
+			<body className="min-h-screen bg-black-100 font-poppins">{children}</body>
+		</html>
+	);
 }
