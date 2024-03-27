310902cd237b4312ac401199df2af4b4eeff9fd2
Tareq <tareqnmd@gmail.com>
2024-03-28 04:42:12 +0600

    header

--- components/Filters.tsx
+++ components/Filters.tsx
@@ -1,54 +1,52 @@
-"use client"
+'use client';
 
 import { formUrlQuery } from '@/sanity/utils';
-import { useState } from 'react'
-import { useSearchParams, useRouter } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useState } from 'react';
 
-const links = ['all', 'Next 13', 'frontend', 'backend', 'fullstack']
+const links = ['all', 'Next 13', 'frontend', 'backend', 'fullstack'];
 
 const Filters = () => {
-  const [active, setActive] = useState('');
-  const searchParms = useSearchParams();
-  const router = useRouter();
+	const [active, setActive] = useState('');
+	const searchParams = useSearchParams();
+	const router = useRouter();
 
-  const handleFilter = (link: string) => {
-    let newUrl = '';
-    
-    if(active === link) {
-      setActive('');
+	const handleFilter = (link: string) => {
+		let newUrl = '';
 
-      newUrl = formUrlQuery({
-        params: searchParms.toString(),
-        keysToRemove: ['category'],
-      })
-    } else {
-      setActive(link);
+		if (active === link) {
+			setActive('');
+			newUrl = formUrlQuery({
+				params: searchParams.toString(),
+				keysToRemove: ['category'],
+			});
+		} else {
+			setActive(link);
+			newUrl = formUrlQuery({
+				params: searchParams.toString(),
+				key: 'category',
+				value: link.toLowerCase(),
+			});
+		}
 
-      newUrl = formUrlQuery({
-        params: searchParms.toString(),
-        key: 'category',
-        value: link.toLowerCase(),
-      })
-    }
-    
-    router.push(newUrl, { scroll: false });
-  }
+		router.push(newUrl, { scroll: false });
+	};
 
-  return (
-    <ul className="text-white-800 body-text no-scrollbar flex w-full max-w-full gap-2 overflow-auto py-12 sm:max-w-2xl">
-      {links.map((link) => (
-        <button
-          key={link}
-          onClick={() => handleFilter(link)}
-          className={`${
-            active === link ?"gradient_blue-purple" : ""
-          } whitespace-nowrap rounded-lg px-8 py-2.5 capitalize`}
-        >
-          {link}
-        </button>
-      ))}
-    </ul>
-  )
-}
+	return (
+		<ul className="text-white-800 body-text no-scrollbar flex w-full max-w-full gap-2 overflow-auto py-12 sm:max-w-2xl">
+			{links.map((link) => (
+				<button
+					key={link}
+					onClick={() => handleFilter(link)}
+					className={`${
+						active === link ? 'gradient_blue-purple' : ''
+					} whitespace-nowrap rounded-lg px-8 py-2.5 capitalize`}
+				>
+					{link}
+				</button>
+			))}
+		</ul>
+	);
+};
 
-export default Filters
+export default Filters;
